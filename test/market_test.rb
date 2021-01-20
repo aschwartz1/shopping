aae879b148474d291759733cd626136c3d26d30f
Alex Schwartz <aschwartz@detaso.com>
2021-01-20 10:31:37 -0600

    Add vendor names method

--- test/market_test.rb
+++ test/market_test.rb
@@ -6,9 +6,10 @@ require './lib/item'
 
 class MarketTest < Minitest::Test
   def setup
-    @farmers_market = Market.new('Farmers Market')
+    @market = Market.new('Farmers Market')
     @garden_seller = Vendor.new('Fruits and Veggies')
     @jelly_seller = Vendor.new('The Preservation Room')
+    @sample_seller = Vendor.new('Free Stuff')
     @peach = Item.new({name: 'Peach', price: '$0.75'})
     @tomato = Item.new({name: 'Tomato', price: '$0.5'})
     @jalepeno = Item.new({name: 'Jalepeno', price: '$0.25'})
@@ -17,11 +18,26 @@ class MarketTest < Minitest::Test
   end
 
   def test_it_exists
-    assert_instance_of Market, @farmers_market
+    assert_instance_of Market, @market
   end
 
   def test_readable_attributes
-    assert_equal 'Farmers Market', @farmers_market.name
-    assert_equal [], @farmers_market.vendors
+    assert_equal 'Farmers Market', @market.name
+    assert_equal [], @market.vendors
+  end
+
+  def test_can_add_vendors
+    @market.add_vendor(@garden_seller)
+    @market.add_vendor(@jelly_seller)
+    @market.add_vendor(@sample_seller)
+
+    assert_equal [@garden_seller, @jelly_seller, @sample_seller], @market.vendors
+  end
+
+  def test_can_tell_vendor_names
+    @market.add_vendor(@garden_seller)
+    @market.add_vendor(@jelly_seller)
+
+    assert_equal ['Fruits and Veggies', 'The Preservation Room'], @market.vendor_names
   end
 end
